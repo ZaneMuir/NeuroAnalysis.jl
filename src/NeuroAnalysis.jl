41f3098a07830a95ad0ec1b25fed82c2b9fe733e
zane muir <zanemuir1995@gmail.com>
2018-11-25 12:59:19 +0800

    update

--- src/NeuroAnalysis.jl
+++ src/NeuroAnalysis.jl
@@ -1,9 +1,11 @@
 module NeuroAnalysis
 
-include("Spike/SpikeUnits.jl")
-include("Spike/FiringRate/LinearFilter.jl")
+# include("Spike/SpikeUnits.jl")
+# include("Spike/FiringRate/LinearFilter.jl")
+include("io.jl")
 
-export SpikeUnits, LinearFilter
+# export SpikeUnits, LinearFilter
+
+export IO
 
 end  # module NeuroAnalysis
-
